95e792bd61490992ab0818dcc5d6ca056058222b
yqw19920724 <986590953@qq.com>
2018-06-05 21:32:57 +0800

    添加登录

---
+++ src/assets/js/http.js
@@ -0,0 +1,67 @@
+import 'whatwg-fetch'
+
+const baseUrl = 'http://localhost:3000/';
+
+const apiList = [
+    {
+        apiName: 'login',
+        method: 'POST',
+        url: 'users/login'
+    }
+]
+
+const setXML = (apiName, id, params) => {
+    const copyApiList = JSON.parse(JSON.stringify(apiList));
+    const index = copyApiList.findIndex(api => {
+        return api.apiName === apiName;
+    })
+    if(index === -1) {
+        return [{err: '未找到相应接口！'}]
+    }
+    const api = copyApiList[index];
+    api.url = `${baseUrl}${api.url}`
+    if(Object.keys(params).length === 0) {
+        params = id;
+    }
+    switch (api.method) {
+        case 'GET':
+        
+            api.url = `${api.url}?`;
+            for (const key in params) {
+                api.url = `${api.url}${key}=${params[key]}&`
+            }
+            api.url = api.url.substr(0, api.url.length -1);
+            break;
+        
+            case 'POST':
+            break;
+        
+        default:
+            break;
+    }
+    const apiParams = {method: api.method, headers: {'Content-Type': 'application/json'}};
+    if(Object.keys(params).length !== 0) {
+        apiParams.body = JSON.stringify(params);
+    }
+    return [null, {apiUrl: api.url,apiParams: apiParams}]
+}
+
+export default (apiName, id = '',params = {}) => {
+    const [err, apiData] = setXML(apiName, id, params);
+    if(err) {
+        return console.log(err)
+    }
+
+    return new Promise(function (resolve, reject) {
+        fetch(apiData.apiUrl, apiData.apiParams)
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log('res:',apiData.apiUrl, responseData);   //网络请求成功返回的数据
+            resolve(responseData);
+        })
+        .catch(err => {
+            console.log('err:',apiData.apiUrl, err);   //网络请求失败返回的数据  
+            reject(err);
+        });
+    });
+}
--- src/pages/components/login/login.js
+++ src/pages/components/login/login.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './login.less';
 import { Input, Button } from 'antd';
+import http from '../../../assets/js/http'
 
 class Login extends Component {
 
     constructor(props){
         super();
         this.props = props;
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
+
+    login = () => {
+        http('login', {
+            username: this.state.username,
+            password: this.state.password,
+        }).then(res => {
+            console.log(res)
+        }).catch(err =>{
+            console.log(err)
+        }) 
+    }
+
+    setUsername = (event) => {
+        this.setState({username: event.target.value})
+    }
+
+    setPassword = (event) => {
+        this.setState({password: event.target.value})
     }
 
     render() {
@@ -15,11 +39,11 @@ class Login extends Component {
             <div className="login-panel">
                 <h3>登录</h3>
                 <div className="body">
-                    <Input placeholder="请输入用户名" />
-                    <Input type="password" placeholder="请输入密码" />
+                    <Input value={this.state.username} onChange={this.setUsername} placeholder="请输入用户名" />
+                    <Input value={this.state.password} onChange={this.setPassword} type="password" placeholder="请输入密码" />
                     <div>
                         <Button>取消</Button>
-                        <Button type="primary">确定</Button>
+                        <Button type="primary" onClick={this.login}>确定</Button>
                     </div>
                 </div>
             </div>
