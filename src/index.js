2ef80b6fffef406144a1fdbefa812ade91164703
DESK-20180707YN\Administrator <yqw19920724@163.com>
2018-09-03 15:24:39 +0800

    添加 raphael 和signal

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ehome from './ehome/ehome';
 
+
+ehome.ehomeInit();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/components/svg/svgDemos/svgDemo1/svgDemo1.js
+++ src/pages/components/svg/svgDemos/svgDemo1/svgDemo1.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import SVG from 'svg.js'
+import Raphael from 'raphael'
+import Signal from 'signals'
 import './svgDemo1.less';
 
 class SvgDemo1 extends Component {
 
+    componentDidMount() {
+        this.createSvg();
+        this.signal = new Signal();
+        this.signal.add(() => {
+            alert(2222)
+        })
+    }
+
+    createSvg = () => {
+        const ele = document.getElementById('svg-demo1')
+        const paper = Raphael( ele, '100%', '100%');
+        const cir = paper.circle(35,25,20).attr({
+            fill: 'red'
+        }).click(() => {
+            this.signal.dispatch()
+        })
+    }
+
     render() {
         return (
-            <div className="svg-demo1">
-                aaaaaaaaaaaaaaaaaa
+            <div id='svg-demo1' className="svg-demo1">
+                
             </div>
         );
     }
