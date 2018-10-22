823a9332dd04db57faabd854cdaf6d06e231080c
DESK-20180707YN\Administrator <yqw19920724@163.com>
2018-10-22 11:20:12 +0800

    删除无效组件

--- src/pages/components/goods/goodsList/goodsList.js
+++ src/pages/components/goods/goodsList/goodsList.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
 import './goodsList.less';
-import miniToolTipCtrl from '../../../../common/widget/miniToolTip/miniToolTip'
 
 class GoodsList extends Component {
 
-    componentDidMount() {
-        this.getMousePosition()
-    }
+    componentDidMount() {}
 
-    getMousePosition= () => {
-        document.addEventListener('mousemove', this._renderMiniTitle)
-    }
-
-    _renderMiniTitle = (event) => {
-        miniToolTipCtrl.create({mouseX: event.clientX, mouseY: event.clientY})
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('mousemove', this._renderMiniTitle)
-        miniToolTipCtrl.destroy()
-    }
+    componentWillUnmount() {}
     
-
-
     render() {
         return (
             <div className="GoodsList">
